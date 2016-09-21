427d862dce2c7a655069341b372ddf2c28bfbc65
sticky4loop <christoffer-ekblom@ChristoffersMBP.lan>
2016-09-21 12:21:48 +0200

    Date issue fix

--- model/DateTime.php
+++ model/DateTime.php
@@ -5,7 +5,7 @@ namespace model;
 class DateTime {
 	public function getDate() {
 		$unixTime = time();
-		$format = "l, \\t\\h\\e j\\t\\h \\o\\f F o, \\T\\h\\e \\t\\i\\m\\e \\i\\s H:i:s";
+		$format = "l, \\t\\h\\e jS \\o\\f F o, \\T\\h\\e \\t\\i\\m\\e \\i\\s H:i:s";
 		return gmdate($format, $unixTime);
 	}
 }
