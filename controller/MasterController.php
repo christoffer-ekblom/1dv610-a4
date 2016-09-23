104228c2007d817deb6ef0eb00068103e5a26349
sticky4loop <christoffer-ekblom@ChristoffersMBP.lan>
2016-09-23 10:06:18 +0200

    Message manipulated cookies

--- controller/MasterController.php
+++ controller/MasterController.php
@@ -7,17 +7,16 @@ require_once('model/Model.php');
 
 class MasterController {
     public function init() {
-    	$login = new \model\Login();
-		$dateTime = new\model\DateTime();
-		$layoutView = new \view\LayoutView();
-    	$loginView = new \view\LoginView();
-    	$dateTimeView = new \view\DateTimeView($dateTime->getdate());
-    	$layoutview = new \view\LayoutView();
+        $login = new \model\Login();
+        $dateTime = new\model\DateTime();
+        $layoutView = new \view\LayoutView();
+        $loginView = new \view\LoginView();
+        $dateTimeView = new \view\DateTimeView($dateTime->getdate());
+        $layoutview = new \view\LayoutView();
         $registerview = new \view\RegisterView();
 
-		$response = $login->response();
-		$isLoggedIn = $_SESSION['isLoggedIn'];
-
-    	$layoutView->render($isLoggedIn, $response, $loginView, $dateTimeView, $registerview);
+        $response = $login->response();
+        $isLoggedIn = $_SESSION['isLoggedIn'];
+        $layoutView->render($isLoggedIn, $response, $loginView, $dateTimeView, $registerview);
     }
 }
