40bd0cedfb4c962381681e33c01009960ac7c226
sticky4loop <christoffer-ekblom@ChristoffersMBP.lan>
2016-09-28 11:57:16 +0200

    Styling

--- View/LayoutView.php
+++ View/LayoutView.php
@@ -4,45 +4,42 @@ namespace View;
 
 class LayoutView {
   public function renderLoginForm($isLoggedIn, LoginView $v, DateTimeView $dtv, $username = null) {
-    echo '<!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="utf-8">
-        <title>Login Example</title>
-      </head>
-      <body>
-        <h1>Assignment 2</h1>
-        <a href="?register">Register a new user</a>
+    echo $this->renderPageHead() .
+        '<a href="?register">Register a new user</a>
         ' . $this->renderIsLoggedIn($isLoggedIn) . '
         <div class="container">
-          ' . $v->response($isLoggedIn, $username) . '
-          ' . $dtv->show() . '
-        </div>
-      </body>
-    </html>
-    ';
+          ' . $v->response($isLoggedIn, $username) .
+          $this->renderPageFoot($dtv);
   }
 
   public function renderRegisterForm(RegisterView $v, DateTimeView $dtv) {
-    echo '<!DOCTYPE html>
+    echo $this->renderPageHead() .
+          '<a href="?">Back to login</a>' .
+          $this->renderIsLoggedIn(false) .
+          '<div class="container">' .
+            $v->repsonse() .
+            $this->renderPageFoot($dtv);
+  }
+
+  private function renderPageHead() {
+    return '<!DOCTYPE html>
     <html>
       <head>
         <meta charset="utf-8">
         <title>Login Example</title>
+        <link rel="stylesheet" type="text/css" href="style/default.css">
       </head>
       <body>
-        <h1>Assignment 2</h1>
-        <a href="?">Back to login</a>
-        ' . $this->renderIsLoggedIn(false) . '
-        <div class="container">
-          ' . $v->repsonse() . '
-          ' . $dtv->show() . '
-        </div>
-      </body>
-    </html>
-    ';
+        <h1>Assignment 2</h1>';
   }
-  
+
+  private function renderPageFoot($dtv) {
+    return $dtv->show() .
+        '</div>
+      </body>
+    </html>';
+  }
+
   private function renderIsLoggedIn($isLoggedIn) {
     if($isLoggedIn) {
       return '<h2>Logged in</h2>';
---
+++ graphics/README.md
@@ -0,0 +1,3 @@
+# graphics
+
+bg.jpg is taken from http://freeimagescollection.com/images/backgrounds/yellow-orange-and-red-tones-for-website-backgrounds-and-wallpapers-f4.jpg
