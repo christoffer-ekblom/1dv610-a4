5606966fc27feb12e0b96c93700953447353e792
sticky4loop <christoffer-ekblom@ChristoffersMBP.lan>
2016-10-20 09:39:44 +0200

    Date time format comment

--- View/DateTimeView.php
+++ View/DateTimeView.php
@@ -10,6 +10,7 @@ class DateTimeView {
 	}
 
 	public function show() {
+		// Matching [DAY_OF_WEEK], the [DAY](st/nd/rd/th) [MONTH] [YEAR], The time is [H:i:s]
 		$format = "l, \\t\\h\\e jS \\o\\f F o, \\T\\h\\e \\t\\i\\m\\e \\i\\s H:i:s";
 		$time = gmdate($format, $this->time);
 		return '<p>' . $time . '</p>';
